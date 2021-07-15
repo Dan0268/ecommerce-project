79869468ce42103a8de94b93af3657b12287a15b
Dan0268 <boskovic.danijel@gmail.com>
2021-07-15 13:11:58 +1000

    Cart barely works now

--- src/containers/Cart/Cart.jsx
+++ src/containers/Cart/Cart.jsx
@@ -24,6 +24,10 @@ const Cart = () => {
     useEffect(() => {
         getData();
     }, []);
+    
+    useEffect(() => {
+        console.log("cartArr changed")
+    },[cartArr])
 
     const itemIndex = (queryItem) => {
         const indexFinder = products.findIndex((item, index) => item.id === queryItem);
@@ -35,25 +39,34 @@ const Cart = () => {
         return variants.findIndex((item, index) => item.variant === queryItem);
     }
 
+    const updateCart = () => {
+        // console.log(cartArr);
+        getCart();
+        // console.log(cartArr);
+    }
+
         
     return (
         <div className={styles.Cart}>
             {cartArr.map((product, index) => {
-                // const productIndex = itemIndex(product.productId);
-                // const varIndex = variantIndex(productIndex, product.variant);
+                const productIndex = itemIndex(product.productId);
+                const varIndex = variantIndex(productIndex, product.variant);
                 // console.log(product.variant),
                 // console.log(productIndex),
                 // console.log(varIndex),
 
                 return (
+                    <>
                     <CartCard
-                        key={index}
-                        product={product} 
-                        quantity={product.quantity}
-                        variant={product.variant}
-                        // imgLink={""}
+                        onChange={updateCart}
+                        key={product.productId}
+                        cartItem={product}
+                        cart={cartArr}
                         imgLink={products[productIndex].variants[varIndex].imgLink}
+                        index={index}
+                        product={products[productIndex]}
                     />
+                </>
                     )
                 }
                 )
